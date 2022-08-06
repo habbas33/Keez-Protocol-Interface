15e9f57364d9fa0a4b708a73470d6c8ce4f6c373
mumeen <azeezmumeen6@gmail.com>
2022-08-06 20:42:58 +0100

    responsive main page

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,29 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { SiEthereum } from "react-icons/si";
 import { IoDiamond } from "react-icons/io5";
-import { AiOutlineClose, AiOutlineLogin, AiOutlineLogout, AiOutlineDown } from "react-icons/ai";
-import { ProfileContext } from '../../context/ProfileContext'
-import { menuItems } from '../../constants/menuItems'
+import {
+  AiOutlineClose,
+  AiOutlineLogin,
+  AiOutlineLogout,
+  AiOutlineDown,
+} from "react-icons/ai";
+import { ProfileContext } from "../../context/ProfileContext";
+import { menuItems } from "../../constants/menuItems";
 import NavItems from "./NavItems";
 import { NavLink } from "react-router-dom";
 import { IPFS_GATEWAY } from "../../constants/globals";
 import Skeleton from "@material-ui/lab/Skeleton";
 
-const NavbarItem = ({title}:any, {classProps}:any) => {
+const NavbarItem = ({ title }: any, { classProps }: any) => {
   return (
-    <li className={`flex items-center mx-10 font-semibold hover:text-[#ac0537] transition duration-300 cursor-pointer ${classProps}`}>
+    <li
+      className={`flex items-center mx-10 font-semibold hover:text-[#ac0537] transition duration-300 cursor-pointer ${classProps}`}
+    >
       {title}
-      {title == "Get Started" &&(
+      {title == "Get Started" && (
         <div className="pl-2">
-          <AiOutlineDown fontSize={12}  />
+          <AiOutlineDown fontSize={12} />
           {/* <div className="flex absolute translate-y-6">
             <a href="#">Create</a>
             <a href="#">Propose</a>
@@ -26,14 +33,15 @@ const NavbarItem = ({title}:any, {classProps}:any) => {
       )}
     </li>
   );
-}
+};
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = React.useState<boolean>(false);
-  const [profileName, setProfileName] = React.useState<string>('');
-  const [profileImageUrl, setProfileImageUrl] = React.useState<string>('');
-  const { accountAddress, disconnectWallet, connectWallet, profileData } = useContext(ProfileContext);
-  let profileImgUrl =profileData?.value?.LSP3Profile?.profileImage[4]?.url;
+  const [profileName, setProfileName] = React.useState<string>("");
+  const [profileImageUrl, setProfileImageUrl] = React.useState<string>("");
+  const { accountAddress, disconnectWallet, connectWallet, profileData } =
+    useContext(ProfileContext);
+  let profileImgUrl = profileData?.value?.LSP3Profile?.profileImage[4]?.url;
 
   useEffect(() => {
     if (!profileData) return;
@@ -49,26 +57,26 @@ export default function Navbar() {
         <div className="flex md:justify-between justify-between items-center">
           <NavLink className="hover:text-[#ac0537]" to={`/`}>
             <div className="flex md:flex flex-initial justify-center items-center">
-                <IoDiamond fontSize={30} color="#ac0537" />
-                <h1 className="text-3xl text-white font-extrabold px-2">KEEZ</h1>
-            </div>     
+              <IoDiamond fontSize={30} color="#ac0537" />
+              <h1 className="text-3xl text-white font-extrabold px-2">KEEZ</h1>
+            </div>
           </NavLink>
-          
-          <ul className="text-white md:flex list-none hidden flex-column justify-between items-center flex-initial">
-            {menuItems.map((menu:any, index:number) => {
-              const depthLevel = 0;
-              return <NavItems items={menu} key={index} depthLevel={depthLevel} />;
-            })}
 
+          <ul className="text-white md:flex list-none hidden flex-column justify-between items-center flex-initial">
+            {menuItems.map((menu: any, index: number) => {
+              const depthLevel = 0;
+              return (
+                <NavItems items={menu} key={index} depthLevel={depthLevel} />
+              );
+            })}
           </ul>
-          
+
           <div className="md:flex flex-initial justify-end items-center">
-            
             <ul className="text-white md:flex hidden list-none flex-row justify-center items-center flex-initial">
               {!accountAddress ? (
                 <button
                   type="button"
-                  onClick={ connectWallet } 
+                  onClick={connectWallet}
                   className="flex flex-row items-center w-44 justify-center text-white font-bold py-2 px-2 rounded bg-[#C3073F] hover:bg-[#ac0537]"
                 >
                   <AiOutlineLogin className="text-white mr-2" />
@@ -76,43 +84,67 @@ export default function Navbar() {
                     Connect Profile
                   </p>
                 </button>
-              ):(
+              ) : (
                 <button
-                type="button"
-                // onClick={ ()=>{console.log("open profile menu")} }
-                onClick={ disconnectWallet }
-                className="flex items-center w-44 justify-center text-white font-bold py-1 px-2 border rounded bg-red border-[#4E4E50] hover:border-[#ac0537]"
-              >
-                {profileImgUrl ?  (
-                  <>
-                    <div className="w-7 h-7 rounded-full border-2 border-light flex justify-center items-center">
-                      <img 
-                        className="object-cover rounded-full "
-                        src={profileImageUrl}
-                      />
-                    </div>
-                    <p className="text-base px-2 font-semibold">
-                      {profileName}
-                    </p>
-                  </>
-                )
-                  :(
+                  type="button"
+                  // onClick={ ()=>{console.log("open profile menu")} }
+                  onClick={disconnectWallet}
+                  className="flex items-center w-44 justify-center text-white font-bold py-1 px-2 border rounded bg-red border-[#4E4E50] hover:border-[#ac0537]"
+                >
+                  {profileImgUrl ? (
                     <>
-                      <Skeleton style={{ backgroundColor: '#4E4E50' }} animation="wave" variant="circle" width={24} height={24} />
-                      <Skeleton style={{ backgroundColor: '#4E4E50',  borderRadius: "25px", marginLeft:"4px"}}  animation="wave" className="px-2" variant="rect" width={110} height={24} />
+                      <div className="w-7 h-7 rounded-full border-2 border-light flex justify-center items-center">
+                        <img
+                          className="object-cover rounded-full "
+                          src={profileImageUrl}
+                        />
+                      </div>
+                      <p className="text-base px-2 font-semibold">
+                        {profileName}
+                      </p>
                     </>
-                  )
-                }
-              </button>
+                  ) : (
+                    <>
+                      <Skeleton
+                        style={{ backgroundColor: "#4E4E50" }}
+                        animation="wave"
+                        variant="circle"
+                        width={24}
+                        height={24}
+                      />
+                      <Skeleton
+                        style={{
+                          backgroundColor: "#4E4E50",
+                          borderRadius: "25px",
+                          marginLeft: "4px",
+                        }}
+                        animation="wave"
+                        className="px-2"
+                        variant="rect"
+                        width={110}
+                        height={24}
+                      />
+                    </>
+                  )}
+                </button>
               )}
             </ul>
           </div>
 
           <div className="flex relative md:hidden">
-            {toggleMenu
-              ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/>
-              : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
-            }
+            {toggleMenu ? (
+              <AiOutlineClose
+                fontSize={28}
+                className="text-white md:hidden cursor-pointer"
+                onClick={() => setToggleMenu(false)}
+              />
+            ) : (
+              <HiMenuAlt4
+                fontSize={28}
+                className="text-white md:hidden cursor-pointer"
+                onClick={() => setToggleMenu(true)}
+              />
+            )}
 
             {toggleMenu && (
               <ul
@@ -121,11 +153,15 @@ export default function Navbar() {
                 "
               >
                 <li className="text-xl w-full my-2">
-                  <AiOutlineClose onClick={() => setToggleMenu(false)}/>
+                  <AiOutlineClose onClick={() => setToggleMenu(false)} />
                 </li>
                 {[].map((item, index) => (
-                  <NavbarItem key={item+index} title={item} classProps="my-2 text-lg"/> 
-            ))}
+                  <NavbarItem
+                    key={item + index}
+                    title={item}
+                    classProps="my-2 text-lg"
+                  />
+                ))}
               </ul>
             )}
           </div>
@@ -135,4 +171,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
