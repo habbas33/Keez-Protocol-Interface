fd72d3b77e8c3efa919e5afc81f1c204eae8082c
mumeen <azeezmumeen6@gmail.com>
2022-08-22 10:17:44 +0100

    responsive about page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,31 +1,38 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 
-import { AboutUsInfo } from '../components/aboutus';
-import {SageMemberCard,B00steMemberCard,HabbasMemberCard,LandonMemberCard,FawnMemberCard,MumeenMemberCard} from '../components/aboutus/TeamMembers';
+import { AboutUsInfo } from "../components/aboutus";
+import {
+  SageMemberCard,
+  B00steMemberCard,
+  HabbasMemberCard,
+  LandonMemberCard,
+  FawnMemberCard,
+  MumeenMemberCard,
+} from "../components/aboutus/TeamMembers";
 // import ReactCardFlip from "react-card-flip";
-
 
 const Main: React.FC = () => {
   return (
     <div className="min-h-screen">
       <div className="bg-welcome">
-        <AboutUsInfo/>
+        <AboutUsInfo />
         <div className="flex-column flex-initial justify-between py-5 px-20">
-          <h1 className="text-3xl text-left text-white font-extrabold">Team Members</h1>
-          </div>
-
-      <div className="bg-welcome flex flex-wrap justify-between items-center px-10">
-        <SageMemberCard/>
-        <B00steMemberCard/>
-        <HabbasMemberCard/>
-        <LandonMemberCard/>
-        <FawnMemberCard/>
-        <MumeenMemberCard/>
-
+          <h1 className="text-3xl text-left text-white font-extrabold">
+            Team Members
+          </h1>
         </div>
-      </ div>
+
+        <div className="bg-welcome grid flex-wrap place-content-stretch gap-3 md:grid-cols-2 lg:grid-cols-3 items-center px-5 md:px-20">
+          <SageMemberCard />
+          <B00steMemberCard />
+          <HabbasMemberCard />
+          <LandonMemberCard />
+          <FawnMemberCard />
+          <MumeenMemberCard />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Main;
